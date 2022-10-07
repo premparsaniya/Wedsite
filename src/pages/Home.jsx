94e315dc23d8f9a74b0ae947b19f4ad11a5e154c
premparsaniya <premparsaniya.ubs@gmail.com>
2022-10-07 14:29:29 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,35 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-      <img src="../images/BG-img.jpg" alt="BG" />
-    </div>
+    <section className="home-page">
+      <div className="home-img">
+        <img
+          src="/content/dam/saudi-tourism/media/see/1920x1080/brand-page-hero.jpg"
+          alt="BG"
+          className="Home-bg-img"
+        />
+      </div>
+      <div className="social-media-icon">
+        <img
+          src="../images/instagram.svg"
+          alt="Instagram Logo"
+          className="social-icon"
+        />
+        <br />
+        <img
+          src="../images/facebook.svg"
+          alt="Instagram Logo"
+          className="social-icon"
+        />
+        <br />
+        <img
+          src="../images/twitter.svg"
+          alt="Instagram Logo"
+          className="social-icon"
+        />
+      </div>
+      <div className="home-card"></div>
+    </section>
   );
 }
 
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Navbar() {
+  const [show, setShow] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+      setLastScrollY(window.scrollY);
+    }
+  };
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlNavbar);
+      return () => {
+        window.removeEventListener("scroll", controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
   return (
-    <header>
-      <section className="navbar">
-        <img src="../images/Logo2.png" alt="Blog Logo" className="logo-img" />
-        <div className="navbar-menu">
-          <span className="navbar-menu-title">HOME</span>
-          <span>FASHION</span>
-          <span>PHOTOGRAPHY</span>
-          <span>FOOD</span>
-        </div>
-      </section>
-    </header>
+    <>
+      <header className="header">
+        <section className={`navbar ${show && "navbar-hidden"}`}>
+          <img src="../images/Logo2.png" alt="Blog Logo" className="logo-img" />
+          <div className="navbar-menu">
+            <span className="navbar-menu-title">HOME</span>
+            <span>FASHION</span>
+            <span>PHOTOGRAPHY</span>
+            <span>FOOD</span>
+          </div>
+        </section>
+      </header>
+    </>
   );
 }
 
