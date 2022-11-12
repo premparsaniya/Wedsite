14892082acc2166f64aee5f473d40adbbabcdedb
premparsaniya <premparsaniya.ubs@gmail.com>
2022-11-12 10:39:52 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 
 function Home() {
+  // var event = undefined;
+  var balls = document.getElementsByClassName("ball");
+  document.onmousemove = function (event) {
+    var x = (event.clientX * 100) / window.innerWidth + "%";
+    var y = (event.clientY * 100) / window.innerHeight + "%";
+    //event.clientX => get the horizontal coordinate of the mouse
+    //event.clientY => get the Vertical coordinate of the mouse
+    //window.innerWidth => get the browser width
+    //window.innerHeight => get the browser height
+
+    for (var i = 0; i < 2; i++) {
+      balls[i].style.left = x;
+      balls[i].style.top = y;
+      balls[i].style.transform = "translate(-" + x + ",-" + y + ")";
+    }
+  };
   return (
     <section className="home-page">
       <div className="home-img">
@@ -35,6 +51,11 @@ function Home() {
           </div>
         </h1>
       </div>
+      <section>
+        <div className="inner">
+          <div class="ball" />
+        </div>
+      </section>
     </section>
   );
 }
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -21,6 +21,7 @@ function Navbar() {
       };
     }
   }, [lastScrollY]);
+  
 
   return (
     <>
